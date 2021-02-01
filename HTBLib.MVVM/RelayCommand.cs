eaaf13ed8d50f835f7465ee78b5158329f75c145
Jan Führer <jan_fue@icloud.com>
2021-02-01 09:54:49 +0100

    added RelayCommand

---
+++ HTBLib.MVVM/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HTBLib.MVVM
+{
+    /// <summary>
+    ///   A command where the function take no parameters
+    /// </summary>
+    public class RelayCommand : RelayCommand<object>
+    {
+        public RelayCommand(Action action, Func<bool> canExecute = null)
+            : base(p => action(), p => (canExecute ?? (() => true)).Invoke())
+        {
+        }
+    }
+
+    /// <summary>
+    ///   A command where the function takes one parameter of type T
+    /// </summary>
+    /// <typeparam name="T">The type of the action parameter</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _action;
+        private readonly Func<T, bool> _canExecute;
+        public RelayCommand(Action<T> action, Func<T, bool> canExecute = null)
+        {
+            _action = action;
+            _canExecute = canExecute ?? (p => true);
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler CanExecuteChanged;
+
+        /// <inheritdoc/>
+        public bool CanExecute(object parameter) => _canExecute((T)parameter);
+
+        /// <inheritdoc/>
+        public void Execute(object parameter) => _action((T)parameter);
+    }
+}
