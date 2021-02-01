31179ccc282ccb4db49ce24ff340e532c5601285
Jan Führer <jan_fue@icloud.com>
2021-02-01 10:13:06 +0100

    added more fiels

---
+++ HTBLib.MVVM.Xamarin/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HTBLib.MVVM.Xamarin
+{
+    /// <inheritdoc/>
+    public abstract class BaseViewModel : MasterBaseViewModel
+    {
+        protected sealed override void InvokeOnMainThread(Action action)
+        {
+            Device.BeginInvokeOnMainThread(action);
+        }
+    }
+}
